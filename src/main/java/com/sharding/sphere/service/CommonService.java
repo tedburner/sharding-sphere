b621ef3f87ee176cc32a6531dc1d88e86c4f844d
tedburner <jlj_vip98@qq.com>
2019-06-10 11:45:50 +0800

    添加查询功能测试

--- src/main/java/com/sharding/sphere/service/CommonService.java
+++ src/main/java/com/sharding/sphere/service/CommonService.java
@@ -1,5 +1,9 @@
 package com.sharding.sphere.service;
 
+import com.sharding.sphere.domain.entity.OrderEntity;
+
+import java.util.List;
+
 /**
  * @author: lingjun.jlj
  * @date: 2019/6/6 11:54
@@ -8,4 +12,6 @@ package com.sharding.sphere.service;
 public interface CommonService {
 
     void save();
+
+    List<OrderEntity> findSomeOne();
 }
